ca96253450779e43aa9a9b801d04e2b566943767
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-02-07 17:54:05 +0100

    feat: screen_util setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sm_iot_lab/constants/colors.dart';
 import 'package:sm_iot_lab/home.dart';
 
 void main() {
@@ -11,14 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SM IoT Lab',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const HomePage(title: 'Home'),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          title: 'SM IoT Lab',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: AppColors.purple),
+            useMaterial3: true,
+          ),
+          home: const HomePage(title: 'Home'),
+        );
+      },
     );
   }
 }
